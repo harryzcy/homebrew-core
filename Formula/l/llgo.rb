f702360ee9d403729cfc82a60cf9ad5871e29d07
Michael Cho <michael@michaelcho.dev>
2026-03-01 12:00:26 -0500

    llgo 0.12.2

--- Formula/l/llgo.rb
+++ Formula/l/llgo.rb
@@ -1,8 +1,8 @@
 class Llgo < Formula
   desc "Go compiler based on LLVM integrate with the C ecosystem and Python"
   homepage "https://github.com/goplus/llgo"
-  url "https://github.com/goplus/llgo/archive/refs/tags/v0.12.1.tar.gz"
-  sha256 "a9ddf42295ab1348e231bdf42a6035f7a17d00ccfa820b4abf3680b222a3afbf"
+  url "https://github.com/goplus/llgo/archive/refs/tags/v0.12.2.tar.gz"
+  sha256 "a52cb2d41805747d0e882d050d6bf6a626b510c8a3d34a2c511a51df54117d43"
   license "Apache-2.0"
   head "https://github.com/goplus/llgo.git", branch: "main"
 
@@ -21,12 +21,9 @@ class Llgo < Formula
     sha256               x86_64_linux:  "e2c6cd5a0254e48ad57576576f12fc0a5d49c7b607fcf2b33f3cacdd742016ac"
   end
 
-  # NOTE: Can be undeprecated if upstream adds support for Go 1.25+
-  deprecate! date: "2026-02-11", because: "uses deprecated go@1.24"
-
-  depends_on "bdw-gc"
-  depends_on "go@1.24"
-  depends_on "libuv"
+  depends_on "bdw-gc" => :no_linkage
+  depends_on "go@1.25"
+  depends_on "libuv" => :no_linkage
   depends_on "lld@19"
   depends_on "llvm@19"
   depends_on "openssl@3"
@@ -50,12 +47,12 @@ class Llgo < Formula
       -s -w
       -X github.com/goplus/llgo/internal/env.buildVersion=v#{version}
       -X github.com/goplus/llgo/internal/env.buildTime=#{time.iso8601}
-      -X github.com/goplus/llgo/xtool/env/llvm.ldLLVMConfigBin=#{llvm.opt_bin/"llvm-config"}
+      -X github.com/goplus/llgo/xtool/env/llvm.ldLLVMConfigBin=#{llvm.opt_bin}/llvm-config
     ]
     tags = nil
     if OS.linux?
       # Workaround to avoid patchelf corruption when cgo is required
-      if Hardware::CPU.arch == :arm64
+      if Hardware::CPU.arm64?
         ENV["CGO_ENABLED"] = "1"
         ENV["GO_EXTLINK_ENABLED"] = "1"
         ENV.append "GOFLAGS", "-buildmode=pie"
@@ -71,25 +68,24 @@ class Llgo < Formula
       tags = "byollvm"
     end
 
-    system "go", "build", *std_go_args(ldflags:, tags:), "-o", libexec/"bin/", "./cmd/llgo"
+    system "go", "build", *std_go_args(ldflags:, tags:, output: libexec/"bin/llgo"), "./cmd/llgo"
 
     libexec.install "LICENSE", "README.md", "go.mod", "go.sum", "runtime"
 
     path_deps = %w[lld go pkgconf].map { |name| find_dep(name).opt_bin }
     path_deps << llvm.opt_bin
-    script_env = { PATH: "#{path_deps.join(":")}:$PATH" }
+    script_env = { PATH: "#{path_deps.join(":")}:${PATH}" }
 
     if OS.linux?
       libunwind = find_dep("libunwind")
-      script_env[:CFLAGS] = "-I#{libunwind.opt_include} $CFLAGS"
-      script_env[:LDFLAGS] = "-L#{libunwind.opt_lib} -Wl,-rpath,#{libunwind.opt_lib} $LDFLAGS"
+      script_env[:CFLAGS] = "-I#{libunwind.opt_include} ${CFLAGS}"
+      script_env[:LDFLAGS] = "-L#{libunwind.opt_lib} -Wl,-rpath,#{libunwind.opt_lib} ${LDFLAGS}"
     end
 
-    (libexec/"bin").children.each do |f|
+    (libexec/"bin").each_child do |f|
       next if f.directory?
 
-      cmd = File.basename(f)
-      (bin/cmd).write_env_script libexec/"bin"/cmd, script_env
+      (bin/f.basename).write_env_script f, script_env
     end
   end
 
@@ -104,7 +100,7 @@ class Llgo < Formula
     bdwgc = find_dep("bdw-gc")
     ENV.prepend_path "LD_LIBRARY_PATH", bdwgc.opt_lib
 
-    (testpath/"hello.go").write <<~GO
+    (testpath/"hello.go").write <<~'GO'
       package main
 
       import (
@@ -119,7 +115,7 @@ class Llgo < Formula
 
       func main() {
         fmt.Println("Hello LLGo by fmt.Println")
-        c.Printf(c.Str("Hello LLGo by c.Printf\\n"))
+        c.Printf(c.Str("Hello LLGo by c.Printf\n"))
       }
     GO
     (testpath/"hello_test.go").write <<~GO
@@ -138,16 +134,14 @@ class Llgo < Formula
     (testpath/"go.mod").write <<~GOMOD
       module hello
     GOMOD
+
+    expected = "Hello LLGo by fmt.Println\nHello LLGo by c.Printf\n"
     system go.opt_bin/"go", "get", "github.com/goplus/lib"
     # Test llgo run
-    assert_equal "Hello LLGo by fmt.Println\n" \
-                 "Hello LLGo by c.Printf\n",
-                 shell_output("#{bin}/llgo run .")
+    assert_equal expected, shell_output("#{bin}/llgo run .")
     # Test llgo build
     system bin/"llgo", "build", "-o", "hello", "."
-    assert_equal "Hello LLGo by fmt.Println\n" \
-                 "Hello LLGo by c.Printf\n",
-                 shell_output("./hello")
+    assert_equal expected, shell_output("./hello")
     # Test llgo test
     assert_match "PASS", shell_output("#{bin}/llgo test .")
   end
